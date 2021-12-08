499f170e55cf018318ad95f3aef8635ba2eda94b
Austin Stitz <82676411+AustinMichael2418@users.noreply.github.com>
2021-12-08 07:59:11 -0500

    Update iframesize.js

--- js/iframesize.js
+++ js/iframesize.js
@@ -1,6 +1,6 @@
 const iframes = document.getElementsByTagName('iframe');
 
 for(let i = 0; i < iframes.length; i++) {
-  iframes[i].style.width = "50%";
-  iframes[i].style.height = "50%";
+  iframes[i].style.width = "100vh";
+  iframes[i].style.height = "50vw";
 }
