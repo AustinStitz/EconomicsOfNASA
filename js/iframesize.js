5f22aff2d0c9f975e7d935bc4bc88560d5c10859
Austin Stitz <82676411+AustinMichael2418@users.noreply.github.com>
2021-12-15 18:58:31 -0500

    Add images

--- js/iframesize.js
+++ js/iframesize.js
@@ -4,3 +4,10 @@ for(let i = 0; i < iframes.length; i++) {
   iframes[i].style.width = "100vh";
   iframes[i].height = (window.innerHeight / 2);//.style.height = "50vh";
 }
+
+const imgs = document.getElementsByTagName('img');
+
+for(let i = 0; i < imgs.length; i++) {
+  imgs[i].style.width = "100vh";
+  imgs[i].height = (window.innerHeight / 2);//.style.height = "50vh";
+}
